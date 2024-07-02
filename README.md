20fd52c273c1ef3e21ac471adfa37ef051690234
Alice Morotti <157374316+alicemorotti@users.noreply.github.com>
2024-07-02 12:20:50 +0200

    Modifica del file "README.md" e completamento del file "calcolatrice.py".
    Co-Authored-By: claudiaa-git <174419695+claudiaa-git@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Progetto-CLAITA02
- 
+Abbiamo una persona senza Visual Studio Code e una persona che ce l'ha, ma non può usare il PC.
+Ci stiamo provando. 
--- calcolatrice.py
+++ calcolatrice.py
@@ -7,13 +7,13 @@ def divisione(num: int | float, den: int | float) -> float:
     return num / den
 
 # Aggiungi le funzioni moltiplicazione e sottrazione
-def sottrazione():
-    pass
+def sottrazione(a: int | float, b: int | float) -> int | float:
+    return a - b
 
-def moltiplicazione():
-    pass
+def moltiplicazione(a: int | float, b: int | float) -> int | float:
+    return a * b
 
 # Aggiungi una funzione magicNumbers per restituire una lista di tutti e soli i numeri interi dispari e multipli di 5 tra start e stop
-def magicNumbers():
+def magicNumbers(start: int | float, stop: int | float) -> list[int | float]:
     # bonus: fallo su una unica riga con la list comprehension
-    pass
+    return [x for x in range(start, stop+1) if x % 2 != 0 and x % 5 == 0]
